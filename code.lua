fe26a8468c41976acc95a067b24f03e42ddf7f4c
ajvernon <a.vernon@outlook.com>
2017-10-26 10:29:17 +0100

    Implemented X-Axis movement for the player

--- code.lua
+++ code.lua
@@ -9,7 +9,7 @@ player1=
     yPos = 16,
     xChange = 0,
     yChange = 0,
-    onGround = true,
+    onGround = false,
     jumpSpeed = 3.0,
 }
 
@@ -20,7 +20,35 @@ function Init()
 end
 
 function Update(deltaTime)
+    local startX = player1.xPos
     
+    -- Initial Y axis movement!
+    if Button(4)
+        and player1.onGround == true then
+            player1.yChange=-player1.jumpSpeed
+    end
+
+    -- Movement on the X axis!
+
+    player1.xChange = 0
+    if Button(2) then
+        player1.xChange = -1
+    end
+    
+    if Button(3) then
+        player1.xChange = 1
+    end
+
+    player1.xPos = player1.xPos + player1.xChange
+   
+    -- Collision on the X axis!
+    
+    local xOffset =0
+    if player1.xChange > 0 then xOffset = 7 end
+
+    if Flag((player1.xPos+xOffset)/8, (player1.yPos+7)/8) == 0 then
+        player1.xPos = startX
+    end
 end
 
 function Draw()
